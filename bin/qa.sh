913235824d4e40c0337b98adc3f22c4b652038b3
Bas <info@laravel-freelancer.nl>
2023-12-03 22:56:14 +0100

    disabled phpmd check
    @see: https://github.com/pdepend/pdepend/issues/695

--- bin/qa.sh
+++ bin/qa.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 printf "\nRun PHPMD\n"
-./vendor/bin/phpmd src/ text phpmd-ruleset.xml
+# disabled for now @see: https://github.com/pdepend/pdepend/issues/695
+#./vendor/bin/phpmd src/ text phpmd-ruleset.xml
 
 printf "\nRun PHPStan\n"
 composer analyse
