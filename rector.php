5ded09799dcc41d2ccac27142c40466571b00967
Laravel Freelancer <info@laravel-freelancer.nl>
2026-03-21 17:27:38 +0100

    Add Rector config

---
+++ rector.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+use Rector\Config\RectorConfig;
+
+return RectorConfig::configure()
+    ->withPaths([
+        __DIR__ . '/config',
+        __DIR__ . '/src',
+        __DIR__ . '/tests',
+    ])
+    ->withPhpSets();
